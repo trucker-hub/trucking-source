c2766adc28c36411460d97fea6644b8f2ae47dd3
jinbochen <jinbochen@voyageones-iMac.local>
2015-08-04 16:04:40 -0700

    cleaned up directive for location

--- client/app/sourcing/sourcing.controller.js
+++ client/app/sourcing/sourcing.controller.js
@@ -5,11 +5,13 @@ angular.module('servicesApp')
 
         $scope.message = 'Hello';
         $scope.shipTo = {
+            label:"shipTo",
             location: "...",
             locationType: 2,
             extraServices: []
         }
         $scope.shipFrom = {
+            label:"shipFrom",
             location: "...",
             locationType: 1,
             extraServices: []
