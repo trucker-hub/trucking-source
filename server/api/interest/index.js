bc5c76bafc67c25dcad14b6ea22671b0177819b6
jinbo chen <jinbo.chen@gmail.com>
2016-01-14 21:01:16 -0800

    add placeholder for Specials

--- server/api/interest/index.js
+++ server/api/interest/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var controller = require('./interest.controller');
-var auth = require('../auth/auth.service');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
